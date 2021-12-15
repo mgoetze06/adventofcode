69f4ddd7e0910530195e06040da8715e0e345c04
Goetze <99mg24@web.de>
2021-12-15 14:53:11 +0100

    advent3_2

--- advent3_2.ps1
+++ advent3_2.ps1
@@ -1,57 +1,95 @@
-﻿$input = Get-Content "input4.txt"
+﻿function Get-CommonBitIsTrue{
+    param($list,$column)
+    $thislist = $list
+    $thiscolumn = $column
+    $zeros = 0
+    $ones = 0
+    $length = ($thislist | Measure-Object).count
+    $oneslist = [System.Collections.ArrayList]@()
+    $zeroslist = [System.Collections.ArrayList]@()
+    for($i = 0; $i -lt $length; $i++){
+        #$thislist[0]
+        #$thislist[1]
+        #$thislist
+        $thislist[$i]
+        $bit = $thislist[$i].Substring($thiscolumn,1)
+        $bit
+        if($bit -eq "0"){
+            $zeros += 1
+            $zeroslist += $i
+        }
+        if($bit -eq "1"){
+            $ones += 1
+            $oneslist += $i
+        }
+    }
+    if($ones -gt $zeros){
+        $true
+        #$oneslist
+    }else{
+        $false
+        #$oneslist
+    }
+    
+}
+
+$input = Get-Content "input4_test.txt"
 $length = ($input | Measure-Object).count
 #$input[0].Substring(0,1)
 $columns =  $input[0].Length
-$columns
-$gamma = ""
-$epsilon = ""
-$list = [System.Collections.ArrayList]@()
+#$columns
+#$list = [System.Collections.ArrayList]@()
+
+$oxy = ""
+$co = ""
 #[convert]::ToString(63,2)
 #[convert]::ToInt32('1000000',2)
 for($j = 0; $j -lt $columns; $j++){
     $zeros = 0
     $ones = 0
-    for($i = 0; $i -lt $length; $i++
-        $bit = $input[$i].Substring($j,1)
-        if($bit -eq "0"){
-            $zeros += 1
-        }
-        if($bit -eq "1"){
-            $ones += 1
-        }
- 
-} 
-Write-output "This is gamma "$gamma
-$gamma_int = [convert]::ToInt32($gamma,2)
-$gamma_int
-$epsilon
-$epsilon_int = [convert]::ToInt32($epsilon,2)
-$epsilon_int
-
-$gamma_int * $epsilon_int
-
-
-
-function CommonBitIsTrue(){
-    param($list,$column)
-    $zeros = 0
-    $ones = 0
-    $length = ($list | Measure-Object).count
-    $returnlist = [System.Collections.ArrayList]@()
+    $length = ($input | Measure-Object).count
+    if($length -eq 1){
+        break
+    }
+    Write-Output "Column: " $j
+    $oneslist = [System.Collections.ArrayList]@()
+    $zeroslist = [System.Collections.ArrayList]@()
     for($i = 0; $i -lt $length; $i++){
         $bit = $input[$i].Substring($j,1)
         if($bit -eq "0"){
             $zeros += 1
+            $zeroslist += $input[$i]
         }
         if($bit -eq "1"){
             $ones += 1
+            $oneslist += $input[$i]
         }
     }
-    if($ones -gt $zeros){
-        $output = $true 
+    $zeroslist
+    $oneslist
+    #clear-variable $input
+    if(($ones -gt $zeros)-or($ones -eq $zeros)){
+        $input = $oneslist
+        $oxy += '1'
+        $co += '0'
+        Write-Output "Common Bit: 1"
+    }else{
+        $input = $zeroslist
+        $oxy += '0'
+        $co += '1'
+        Write-Output "Common Bit: 0"
     }
-    return $output
+    # clear-variable $oneslist $zeroslist -ErrorAction SilentlyContinue
 }
+$oxy
+$co
+#$gamma_int = [convert]::ToInt32($gamma,2)
+#$epsilon_int = [convert]::ToInt32($epsilon,2)
+
+
+
+
+
 
 
 #0
