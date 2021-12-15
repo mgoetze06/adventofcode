97eb32df18cda6bf03bf977f53bb10e2218f32b0
mgoetze06 <75042211+mgoetze06@users.noreply.github.com>
2021-12-15 10:55:49 +0100

    Add files via upload

---
+++ advent1.ps1
@@ -0,0 +1,14 @@
+﻿$input = Get-Content "input.txt"
+$length = ($input | Measure-Object).count
+$increasing = 1
+for($i = 0; $i -lt $length; $i++){
+    if($i -gt 0){
+        if($input[$i] -gt $input[$i-1]){
+            #$input[$i-1]
+            $increasing = $increasing + 1
+            #Write-Host "Increasing"
+            #$input[$i]
+        }
+    }
+} 
+$increasing
---
+++ advent4.ps1
@@ -0,0 +1,12 @@
+﻿$input = Get-Content "input4.txt"
+$length = ($input | Measure-Object).count
+$input[0].Substring(0,1)
+
+
+[convert]::ToString(63,2)
+
+[convert]::ToInt32('1000000',2)
+
+for($i = 0; $i -lt $length; $i++){
+
+} 
