504448d84fdf4835a2c25a991a41ba48f0854f5d
Goetze <99mg24@web.de>
2021-12-15 12:50:22 +0100

    new files

---
+++ advent5.ps1
@@ -0,0 +1,22 @@
+﻿$input = Get-Content "input4.txt"
+$length = ($input | Measure-Object).count
+$input[0].Substring(0,1)
+
+#small changes
+
+
+#sajdkbahskjdbakjsdb
+[convert]::ToString(63,2)
+
+[convert]::ToInt32('1000000',2)
+newfunction
+for($i = 0; $i -lt $length; $i++){
+    newfunction
+} 
+
+
+
+function newfunction{
+    Write-Output "test"
+
+}
