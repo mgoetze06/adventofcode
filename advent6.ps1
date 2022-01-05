2fb1d0831a6b3ae16ea419324a1a6ccf1a976602
Goetze <99mg24@web.de>
2022-01-05 17:28:23 +0100

    advent6 started

---
+++ advent6.ps1
@@ -0,0 +1,28 @@
+﻿$input = Get-Content "input6.txt"
+$fish = [System.Collections.ArrayList]@()
+$fish = $input.Split(",")
+#$fish -join ""
+
+for($i = 0; $i -lt 80; $i++){
+    $newFish = 0
+    for($f = 0; $f -lt ($fish | Measure-Object).count; $f++){
+        #count how many 0 there are 
+        if($fish[$f] -eq 0){
+            $newFish += 1
+            $fish[$f] = 6
+        }else{
+            $fish[$f] -= 1
+        }
+        #decrease each fish by one
+         
+
+    }
+    
+    #add the amount of zeros as eigths to the array
+    for($n = 0; $n -lt $newFish; $n++){
+        $fish += 8
+    }
+    Write-Output "Iteration: $i"
+    ($fish | Measure-Object).count
+    #$fish -join ""
+}
