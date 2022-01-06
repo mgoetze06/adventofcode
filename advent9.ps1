3520a01d7ddf880f7aceb5ddcdac727938d60656
Maurice <99mg24@web.de>
2022-01-06 16:31:08 +0100

    advent9 part 1 complete

--- advent9.ps1
+++ advent9.ps1
@@ -3,17 +3,27 @@ $global:low_points = [System.Collections.ArrayList]@()
 $global:count = 0
 function checkN($i,$j){
     Write-Output "checking neighbor of $i $j"
+    $i = [int]$i
+    $j = [int]$j
     $current = $global:input[$i][$j]
-    $temp = $i-1
-    $i_before = $global:input[$temp][$j]
-    $temp = $i +1
-    $i_next = $global:input[$temp][$j]
-    $temp = $j -1
-    $j_before = $global:input[$i][$temp]
-    $temp = $j +1
-    $j_next = $global:input[$i][$temp]
-    $idiff = 0
-    $jdiff = 0
+    if($i -gt 0){
+        $temp = $i-1
+        $i_before = $global:input[$temp][$j]
+    }
+    if($i -lt 99){
+        $temp = $i+1
+        $i_next = $global:input[$temp][$j]
+    }
+    if($j -gt 0){
+        $temp = $j-1
+        $j_before = $global:input[$i][$temp]
+    }
+    if($j -lt 99){
+        $temp = $j+1
+        $j_next = $global:input[$i][$temp]
+    }
+    [int]$idiff = 0
+    [int]$jdiff = 0
 
     if($i -eq 0){
         if($j -eq 0){
@@ -131,15 +141,45 @@ function checkN($i,$j){
             #if($j_next -lt $current){
             #   $jdiff = 1 
             #}
+    }else{
+        #alles andere
+            if($i_next -lt $current){
+               $idiff = 1 
+            }
+            if($i_before -lt $current){
+               $idiff = -1 
+            }
+            if($j_before -lt $current){
+               $jdiff = -1 
+            }
+            if($j_next -lt $current){
+               $jdiff = 1 
+            }
+
+
     }
 
 
 
     if(($idiff -eq 0)-and($jdiff -eq 0)){
-        $global:low_points += ,@($i,$j)    
+        $alreadyThere = $false
+        $length = ($global:low_points| Measure-Object).Count
+        :outer for($k = 0; $k -lt $length; $k++){
+            if(($global:low_points[$k][0] -eq $i)-and($global:low_points[$k][1] -eq $j)){
+                $alreadyThere = $true
+                break outer
+            }
+
+        }
+        if($alreadyThere -eq $false){
+            $global:low_points += ,@($i,$j) 
+        }      
     }else{
-        $tempi = $i+$idiff
-        $tempj = $j+$jdiff
+        [int]$tempi = $i + $idiff
+        [int]$tempj = $j + $jdiff
+        $tempi
+        $tempj
+        ($global:low_points| Measure-Object).Count
         checkN $tempi $tempj
     }
 
@@ -210,12 +250,36 @@ $saved_index_array[99]
 for($i = 0; $i -lt $rows; $i++){
     $length = ($saved_index_array[$i] | Measure-Object).Count
     for($j = 0; $j -lt $length; $j++){
-        checkN $i  $saved_index_array[$i][$j]
+        checkN $i $saved_index_array[$i][$j]
     }
 }
 #$global:low_points
 ($global:low_points| Measure-Object).Count
-#$global:low_points[1]
+$global:low_points[0]
+$global:low_points[1]
+$global:low_points[2]
+$global:low_points[3]
 #$global:low_points[1][0]
 #$global:low_points[1][1]
 #$global:count
+
+#check for doubled low points
+
+$unique = [System.Collections.ArrayList]@()
+$length = ($global:low_points| Measure-Object).Count
+$total = 0
+Write-Output "calculating total"
+for($i = 0; $i -lt $length; $i++){
+    Write-Output "koord"
+    #[int]$global:low_points[$i][0]
+    #[int]$global:low_points[$i][1]
+    #$input[3][0]
+    $currentHeight = [convert]::ToInt32($input[$global:low_points[$i][0]][$global:low_points[$i][1]],10)
+    Write-Output "currentheigt"
+    $currentHeight
+    Write-Output "currentheigt + 1"
+    $currentHeight += 1
+    $currentHeight
+    $total += $currentHeight
+}
+$total
