1d6c0fbdc08d50f2b566c27915fe40c0f151c15d
mgoetze06 <99mg24@web.de>
2024-11-25 10:30:43 +0100

    day 4 complete

--- 2023/41.py
+++ 2023/41.py
@@ -16,9 +16,13 @@ print("total rows: ",len(lines))
 print("total cols: ",len(lines[0]))
 total_score = 0 
 instances_of_cards = dict()
-for row,line in enumerate(lines):
-    print(line)
 
+for i in range(len(lines)):
+    instances_of_cards.update({i:1})
+
+for row,line in enumerate(lines):
+    #print(line)
+    
     numberString = line.split(": ")[1]
     cardString = line.split(": ")[0]
     cardID = getCardID(cardString)
@@ -29,7 +33,7 @@ for row,line in enumerate(lines):
     numbers = splitNumbers(line_split[1])
 
     print("card id: ", cardID)
-
+    print("row: ",row)
     #print("winning numbers: ", winning_numbers)
     #print("my numbers: ", numbers)
 
@@ -37,34 +41,21 @@ for row,line in enumerate(lines):
     for winning_number in winning_numbers:
         if winning_number in numbers:
             total_matching_numbers_per_card += 1
-    instances_of_cards[1] = 1
-    print("totalWinnersPerCard",total_matching_numbers_per_card)
-    if total_matching_numbers_per_card > 0:
-        for i in range(0,total_matching_numbers_per_card):
-            
-                calculatedCardID = cardID + i + 1
-                print("calculated card ID Winner: ",calculatedCardID)
+    #print("totalWinnersPerCard",total_matching_numbers_per_card)
+    for i in range(1,total_matching_numbers_per_card + 1):
+        #print(i)
+        calculatedCardID = row + i
+        #print("calculated row ID Winner: ",calculatedCardID)
 
-                currentWinnerPerCard = instances_of_cards.get(cardID)
-                currentWinnerCalculatedCardID = instances_of_cards.get(calculatedCardID)
+        currentWinnerPerCard = instances_of_cards.get(row)
+        currentWinnerCalculatedCardID = instances_of_cards.get(calculatedCardID)
 
-
-                print("current score: ",currentWinnerPerCard)
-                if currentWinnerPerCard == None:
-                    currentWinnerPerCard = 1
-                if currentWinnerCalculatedCardID == None:
-                    currentWinnerCalculatedCardID = 1
-                instances_of_cards.update({calculatedCardID:currentWinnerPerCard+currentWinnerCalculatedCardID})
-                #print(instances_of_cards)
-    else:
-        currentWinnerPerCard = instances_of_cards.get(cardID)
-        if currentWinnerPerCard == None:
-            currentWinnerPerCard = 1
-        instances_of_cards.update({cardID:currentWinnerPerCard})
+        instances_of_cards.update({calculatedCardID:currentWinnerPerCard+currentWinnerCalculatedCardID})
+        #print(instances_of_cards)
 total = 0
 print(instances_of_cards)
 for value in instances_of_cards.values():
-    print(value)
+    #print(value)
     total += value
 
 print(total)
