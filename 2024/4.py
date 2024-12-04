0e85fde439d3db6abc2e0443d86b14da0f01dd82
Maurice Götze <99mg24@web.de>
2024-12-04 19:01:16 +0100

    day 4 part 1 done

--- 2024/4.py
+++ 2024/4.py
@@ -1,29 +1,39 @@
 import re
-with open('./input/4_val.txt') as f:
+with open('./input/4.txt') as f:
     lines = f.readlines()
 print("total rows: ",len(lines))
 #print(lines[0])
-lines = [l.strip() for l in lines]
+
+#lines = ["....." + l.strip() + "....." for l in lines]
+lines = ["....." + l + "....." for l in lines]
+
+paddingRow = "".join(["." for l in lines[0]])
+print(paddingRow)
+for i in range(5):
+    lines.insert(0,paddingRow)
+    lines.append(paddingRow)
+
+#lines = "."*len(lines)
 #lines = "".join(lines)
 
 def getCharFromPosition(localposition,lines):
     maxRow = len(lines)-1
     maxCol = len(lines[0])-1
     
-    if len(localposition) != 2:
-        return "E"
+    # if len(localposition) != 2:
+    #     return "E"
 
-    if localposition[0] < 0:
-        return "E"
+    # if localposition[0] < 0:
+    #     return "E"
     
-    if localposition[0] > maxRow:
-        return "E"
+    # if localposition[0] > maxRow:
+    #     return "E"
     
-    if localposition[1] < 0:
-        return "E"
+    # if localposition[1] < 0:
+    #     return "E"
     
-    if localposition[1] > maxCol:
-        return "E"
+    # if localposition[1] > maxCol:
+    #     return "E"
     
     #print(len(lines))
     #print(len(lines[0]))
@@ -32,10 +42,10 @@ def getCharFromPosition(localposition,lines):
     return lines[localposition[0]][localposition[1]]
 
 def addDirectionToPosition(localposition,direction):
-    if len(localposition) != 2:
-        return "E"
-    if len(direction) != 2:
-        return "E"
+    #if len(localposition) != 2:
+    #    return "E"
+    #if len(direction) != 2:
+    #    return "E"
     
     newposition = [localposition[0]+direction[0],localposition[1]+direction[1]]
     #print("newposition",newposition)
@@ -51,7 +61,7 @@ for indexRow,line in enumerate(lines):
     indicesOfX = re.finditer(r"X",line)
     indicesOfX = [ind.start() for ind in indicesOfX]
     for index in indicesOfX:
-        print(index)
+        #print(index)
         allXPositions.append([indexRow,index])
     output = ""
     for indexCol,char in enumerate(line):
@@ -65,14 +75,14 @@ for indexRow,line in enumerate(lines):
     print(output)
 
 
-print(allXPositions)
+#print(allXPositions)
 #directions = [[1,0],[1,1],[1,-1]]
 directions = []
-chars = ['X','M','A','S']
+chars = ['X','M','A','S','']
 for rowOffset in range(-1,2,1):
     for colOffset in range(-1,2,1):
         directions.append([rowOffset,colOffset])
-print(directions)
+#print(directions)
 xmasFound = 0
 for position in allXPositions:
     print("")
@@ -86,7 +96,7 @@ for position in allXPositions:
         charAtPosition = getCharFromPosition(positionToCheck,lines)
         print("char at position",charAtPosition)
         while  charAtPosition == chars[charsFound] and not found:
-            #print("charsFound ",charsFound)
+            print("charsFound ",charsFound)
             positionToCheck = addDirectionToPosition(positionToCheck,direction)
             print("newposition",positionToCheck)
             charAtPosition = getCharFromPosition(positionToCheck,lines)
@@ -96,6 +106,7 @@ for position in allXPositions:
                 print("found XMAS in direction",direction)
                 xmasFound += 1
                 found = True
+            
 
         #if charsFound == len(chars)-1:
             
