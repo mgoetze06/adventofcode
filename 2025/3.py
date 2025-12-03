b618f7f7aa83b912964767ee82637d34322fdb5a
Maurice Götze <99mg24@web.de>
2025-12-03 21:45:13 +0100

    day3; output too low; replacing one digit a time

--- 2025/3.py
+++ 2025/3.py
@@ -1,4 +1,7 @@
 from utils_2025 import *
+import re
+
+
 debug = False
 
 with open('input\\3_val.txt') as f:
@@ -17,18 +20,83 @@ def day3_part1(lines):
         stringWithoutLastChar = line[:len(line)-1]
 
         indexFirstChar = findIndexOfBiggestDigitInString(stringWithoutLastChar)
+        printDebug(indexFirstChar,debug)
         biggestFirstChar = stringWithoutLastChar[indexFirstChar]
+        printDebug(biggestFirstChar,debug)
         remainingString = line[indexFirstChar+1:]
+        printDebug(remainingString,debug)
         indexSecondChar = findIndexOfBiggestDigitInString(remainingString)
-        biggestSecondChar = line[indexSecondChar]
+        printDebug(indexSecondChar,debug)
+        biggestSecondChar = remainingString[indexSecondChar]
+        printDebug(biggestSecondChar,debug)
 
         joltage = int(str(biggestFirstChar+biggestSecondChar))
+        printDebug(joltage,debug)
         totalJoltage += joltage
 
     print(totalJoltage)
 
-def day3_part2(lines):
-    print(lines)
+def deaktiviereStringErmittleMaximum(deaktiviert_Index, stringToCheck):
+    maxValue = 0
+    originalString = stringToCheck
+    if len(deaktiviert_Index)==0:
+        return 0
+    charToReplace = stringToCheck[deaktiviert_Index[0]]
+    string = "0"
+    for performReplacement in range(1,len(deaktiviert_Index)):
+        string = originalString.replace(charToReplace,'',performReplacement)
+        #print(string)
+        if len(string)<12:
+            return 0
+        if len(string)==12:
+            break
+        
+        if len(string)>12:
+            string = string[0:12]
 
-day3_part1(lines)
+        value = int(string)
+        if value > maxValue:
+            maxValue = value
+    value = int(string)
+    if value > maxValue:
+        maxValue = value
+    return maxValue
+
+
+
+def day3_part2(lines):
+    totalJoltage = 0
+    for line in lines:
+        print("")
+        print("neue Zeile:" , line)
+        maxValue = 0
+        for i in range(9,0,-1):
+            print("")
+            print("startpunktNum",i)
+            startpunkte = [match.start() for match in re.finditer(str(i), line)]
+            startpunkte.sort(reverse=True)
+            print("startpunkte: ",startpunkte)
+            for startpunkt in startpunkte:
+                stringToCheck = line[startpunkt:]
+                for deaktiviertesDigit in range(1,9):
+                    deaktiviert = [match.start() for match in re.finditer(str(deaktiviertesDigit), stringToCheck)]
+                    value = deaktiviereStringErmittleMaximum(deaktiviert,stringToCheck)
+                    if value > maxValue:
+                        maxValue = value
+                        print("maxvalue: ",maxValue)
+        totalJoltage += maxValue
+    
+    print(totalJoltage)
+    return
+
+#day3_part2 notes
+# 12 digit output
+# start checking at biggest digit, where more than 11 chars to the right
+# always only check to the right
+# start with lowest digit in remaining string
+#   disable from start to the right only this digit
+#    
+
+
+#day3_part1(lines)
 day3_part2(lines)
