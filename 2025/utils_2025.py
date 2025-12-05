6eaa129f333a6c592f8cc285347b89a716e3989e
Maurice Götze <maurice.goetze@siemens.com>
2025-12-05 07:01:03 +0100

    utils updated

--- 2025/utils_2025.py
+++ 2025/utils_2025.py
@@ -1,3 +1,22 @@
 def printDebug(text,debug=False):
     if debug == True:
         print(text)
+
+    
+def findIndexOfBiggestDigitInString(string):
+    n = 1
+    parts = [int(string[i:i+n]) for i in range(0, len(string), n)]
+    index = string.index(str(max(parts)))
+    return index
+
+def isSymbol(char,compareChar):
+    if char == compareChar:
+        return True
+    else:
+        return False
+
+def isDigit(char):
+    if(char == "1" or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9" or char == "0"):
+        return True
+    
+    return False
