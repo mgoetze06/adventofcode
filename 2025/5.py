8c26b965aa9f5b35c1f5d2990f18dd2573d63fe7
Maurice Götze <99mg24@web.de>
2025-12-07 19:00:17 +0100

    day 5 no result

--- 2025/5.py
+++ 2025/5.py
@@ -68,31 +68,51 @@ def day5_part1(lines):
     return
 
 def day5_part2(lines):
-    ingredients = []
+    #ingredients = []
     rules = []
     for line in lines:
         line = line.strip()
         if "-" in line:
             rule = [int(x) for x in line.split("-")]
             rules.append(rule)
-        else:
-            if line != None and line != "":
-                ingredients.append(int(line))
+        #else:
+        #    if line != None and line != "":
+        #        ingredients.append(int(line))
 
     validIngredients = 0
     validRanges = []
     for id,rule in enumerate(rules):
         lowerEdge = rule[0]
         higherEdge = rule[1]
+        newLowerEdge = lowerEdge
+        newHigherEdge = higherEdge
+        print("checking rule:", rule)
         if len(validRanges) == 0:
             validRanges.append(rule)
+            continue
+        addingToValidRange = True
         for validRange in validRanges:
-            if lowerEdge < validRange[1]:
-                newLowerEdge = validRange[1]
-            if higherEdge > validRange[0]:
-                newHigherEdge = validRange[0]
-        
+            if lowerEdge < validRange[0] and higherEdge > validRange[1]:
+                print("removing range because it is into other range",validRange)
+                validRanges.remove(validRange)
+                addingToValidRange = True
+            if lowerEdge >= validRange[0] and higherEdge <= validRange[1]:
+                addingToValidRange = False
 
+            if lowerEdge < validRange[1] and lowerEdge > validRange[0] and higherEdge > validRange[1]:
+                newLowerEdge = validRange[1] + 1
+                print("new lower edge:",newLowerEdge)
+            if higherEdge > validRange[0] and higherEdge < validRange[1] and lowerEdge < validRange[0]:
+                newHigherEdge = validRange[0] - 1
+                print("new higher edge:",newHigherEdge)
+        if addingToValidRange:
+            validRanges.append([newLowerEdge,newHigherEdge])
+        #print(validRanges)
+    for validRange in validRanges:
+        adding = 0
+        if validRange[1] >= validRange[0]:
+            adding = (validRange[1]-validRange[0]) + 1
+        validIngredients += adding
 
     print(validIngredients)
     return
