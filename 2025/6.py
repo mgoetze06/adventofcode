7c1ed94b6c1b0af70f2f848a5aa4b92ca15d58b6
Maurice Götze <99mg24@web.de>
2025-12-09 16:24:12 +0100

    day complete

--- 2025/6.py
+++ 2025/6.py
@@ -71,34 +71,61 @@ def day6_part1(lines):
     print(total)      
     return
 
+def getMaximumLength(column):
+    maxLength = 0
+    for item in column:
+        length = len(item.replace(" ",""))
+        if length > maxLength:
+            maxLength = length
+    return maxLength
+
+def buildOperators(column,length):
+    operators = []
+    for index in range(length):
+        digitString = ""
+        for item in column:
+            if "*" in item or "+" in item:
+                continue
+            if item[index] != None and item[index] != " ":
+                digitString = digitString + item[index]
+
+        operators.append(int(digitString))
+    return operators
+
+def computeColumnResult(column):
+    length = getMaximumLength(column)
+    operators = buildOperators(column,length)
+    operation = column[-1].replace(" ","")
+    if operation == '*':
+        result = 1
+        for i in range(len(operators)):
+            result = result*operators[i]
+    else:
+        if operation == '+':
+            result = 0
+            for i in range(len(operators)):
+                result += operators[i]
+        else:
+
+            print("no operation found")
+            return 0
+        
+    return result
+
 def day6_part2(lines):
     input = []
     total = 0
-    for id,line in enumerate(lines):
-        #print(line.split(" "))
-        filteredLine = line.split(" ")
-        parsedInput = list(filter(None, filteredLine))
-        #print(parsedInput)
-        input.append(parsedInput)
-        print(len(parsedInput))
-        #print(input)
-
-    for index in range(len(input[0])):
-        operators = []
-        operation = input[4][index]
-        for i in range(4):
-            operators.append(input[i][index])    
-
-
-        if operation == '*':
-            result = 0
-            #result = operators[0] * operators[1] * operators[2] * operators[3]
-        else:
-            result = 0
-            #result = operators[0] + operators[1] + operators[2] + operators[3]
-
-        total += result
-    print(total)     
+    columnStartIndex = 0
+    for index,char in enumerate(lines[0]):
+        if char == " " and lines[1][index] == " " and lines[2][index] == " " and lines[3][index] == " ":
+            column = [lines[0][columnStartIndex:index],lines[1][columnStartIndex:index],lines[2][columnStartIndex:index],lines[3][columnStartIndex:index],lines[4][columnStartIndex:index]]
+            columnStartIndex = index + 1
+            result = computeColumnResult(column)
+            total += result
+    column = [lines[0][columnStartIndex:],lines[1][columnStartIndex:],lines[2][columnStartIndex:],lines[3][columnStartIndex:],lines[4][columnStartIndex:]]
+    result = computeColumnResult(column)
+    total += result
+    print(total)
     return
 
 day6_part1(lines)
